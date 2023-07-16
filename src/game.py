22a1beec19a60c95b7a6bcad4e00552667cc5177
LATL9 <113098926+LATL9@users.noreply.github.com>
2023-07-16 12:11:01 +0100

    reinit bullet if off-screen and adjust bullet velocities

--- src/game.py
+++ src/game.py
@@ -15,10 +15,10 @@ class Game:
     def __init__(self):
         self.bullets = [
             Bullet(
-                random.randint(0, screenW - 1),
+                random.randint(0, screenW - 11),
                 0,
                 random.randint(-4, 4),
-                random.randint(0, 8),
+                random.randint(0, 6),
                 12
             ) for i in range(64)
         ]
@@ -26,7 +26,19 @@ class Game:
         self.player = Player(math.floor(screenW / 2), screenH - 64, 24)
 
     def Update(self):
-        for i in range(len(self.bullets)): self.bullets[i].Update()
+        for i in range(len(self.bullets)): 
+            self.bullets[i].Update()
+            if self.bullets[i].x <= self.bullets[i].size * -1 or \
+                self.bullets[i].x >= screenW or \
+                self.bullets[i].y <= self.bullets[i].size * -1 or \
+                self.bullets[i].y >= screenH:
+                    self.bullets[i] = Bullet(
+                        random.randint(0, screenW - 1),
+                        0,
+                        random.randint(-4, 4),
+                        random.randint(0, 8),
+                        12
+                    )
         self.player.Update()
 
     def Draw(self):
