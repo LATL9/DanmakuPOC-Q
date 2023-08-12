dfc113cfba2a9ca72163c751fc17a35fb226a483
Zolax9 <87378963+Zolax9@users.noreply.github.com>
2023-08-12 13:46:09 +0100

    mutation algorithm
    how it works is explained in main.py, but tldr the models are sorted into 4 quarters, with 1st quarter having the most fitness and 4th quarter the least.
    
    4th quarter is randomised, 3rd quarter is replaced by mutated 1st quarter, and 2nd quarter is mutated.
    
    this method in my opinion greatly improves methods used by the top-performing models while allowing completely different methods by random chance to survive and eventually reproduce.

--- src/common.py
+++ src/common.py
@@ -3,10 +3,11 @@ import threading
 WIDTH = 800
 HEIGHT = 800
 NUM_BULLETS = 32
-NUM_PROCESSES = 12
-NUM_MODELS = 24 # MUST BE EVEN
+NUM_PROCESSES = 2
+NUM_MODELS = 4 # must be divisible by 4 (to divide into exact quarters)
 NUM_MODELS_PER_PROCESS = round(NUM_MODELS / NUM_PROCESSES)
-TRAIN_TIME = 20
+TRAIN_TIME = 20 # seconds
+MUTATION_POWER = 0.04 # measure mutations change model
 
 class ThreadWithResult(threading.Thread):
     def __init__(self, group=None, target=None, name=None, args=(), kwargs={}, *, daemon=None):
--- src/main.py
+++ src/main.py
@@ -11,6 +11,24 @@ def train():
     fitnesses = {}
     threads = []
    
+    for i in range(NUM_PROCESSES):
+        threads.append(ThreadWithResult(target=test, args=(device, list(range(i * NUM_MODELS_PER_PROCESS, (i + 1) * NUM_MODELS_PER_PROCESS)), conv1s[i], conv2s[i], outs[i],)))
+    for i in range(len(threads)): 
+        threads[i].start()
+    for i in range(len(threads)):
+        threads[i].join()
+        fitnesses = {**fitnesses, **threads[i].result}
+    return fitnesses
+
+def test(device, indexes, _conv1s, _conv2s, _outs): # _ presents naming conflict
+    r = {}
+    for i in range(NUM_MODELS_PER_PROCESS):
+        m = NNModel(device, indexes[i], _conv1s[i], _conv2s[i], _outs[i])
+        r[indexes[i]] = m.train()
+    return r
+
+if __name__ == '__main__':
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     conv1s = [
         [nn.Sequential(
             nn.Conv2d(1, 1, 16, 1, 2),
@@ -32,33 +50,63 @@ def train():
         for j in range(NUM_PROCESSES)
     ]
 
-    for i in range(NUM_PROCESSES):
-        threads.append(ThreadWithResult(target=test, args=(device, list(range(i * NUM_MODELS_PER_PROCESS, (i + 1) * NUM_MODELS_PER_PROCESS)), conv1s[i], conv2s[i], outs[i],)))
-    for i in range(len(threads)): 
-        threads[i].start()
-    for i in range(len(threads)):
-        threads[i].join()
-        fitnesses = {**fitnesses, **threads[i].result}
-    return fitnesses
+    epoch = 0
 
-def test(device, indexes, conv1s, conv2s, outs):
-    r = {}
-    for i in range(NUM_MODELS_PER_PROCESS):
-        m = NNModel(device, indexes[i], conv1s[i], conv2s[i], outs[i])
-        r[indexes[i]] = m.train()
-    return r
+    while True:
+        epoch += 1
 
-if __name__ == '__main__':
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    epoch = 1
+        fitnesses = train()
+        fitnesses = {k: v for k, v in sorted(fitnesses.items(), key=lambda item: item[1], reverse=True)}
+        vals = list(fitnesses.values())
 
-    fitnesses = train()
-    fitnesses = {k: v for k, v in sorted(fitnesses.items(), key=lambda item: item[1], reverse=True)}
-    vals = list(fitnesses.values())
-    print("Epoch {}: Mean = {}, Median = {}, Top 50% = {}, Bottom 50% = {}".format(
-        epoch,
-        sum(fitnesses.values()) // len(fitnesses),
-        vals[(len(fitnesses) + 1) // 2],
-        sum(vals[:len(fitnesses) // 2]) // (len(fitnesses) // 2),
-        sum(vals[len(fitnesses) // 2:]) // (len(fitnesses) // 2)
-    ))
+        # mutation (1st quarter = highest fitness, 4th quarter = lowest fitness):
+        # 4th quarter is replaced with new random weights
+        # 3rd quarter is replaced by 1st quarter with mutations
+        # 2nd quarter is mutated
+        # 1st quarter stays same
+        keys = list(fitnesses.keys())
+        
+        # 2nd quarter
+        for i in range(NUM_MODELS // 4, NUM_MODELS // 2):
+            for param in conv1s[i // NUM_MODELS_PER_PROCESS][i % NUM_MODELS_PER_PROCESS].parameters():
+                param.data += MUTATION_POWER * torch.randn_like(param)
+            for param in conv2s[i // NUM_MODELS_PER_PROCESS][i % NUM_MODELS_PER_PROCESS].parameters():
+                param.data += MUTATION_POWER * torch.randn_like(param)
+            for param in outs[i // NUM_MODELS_PER_PROCESS][i % NUM_MODELS_PER_PROCESS].parameters():
+                param.data += MUTATION_POWER * torch.randn_like(param)
+
+        # 3rd quarter
+        for i in range(NUM_MODELS // 4):
+            conv1s[(i + NUM_MODELS // 2) // NUM_MODELS_PER_PROCESS][(i + NUM_MODELS // 2) % NUM_MODELS_PER_PROCESS] = conv1s[i // NUM_MODELS_PER_PROCESS][i % NUM_MODELS_PER_PROCESS]
+            conv2s[(i + NUM_MODELS // 2) // NUM_MODELS_PER_PROCESS][(i + NUM_MODELS // 2) % NUM_MODELS_PER_PROCESS] = conv2s[i // NUM_MODELS_PER_PROCESS][i % NUM_MODELS_PER_PROCESS]
+            outs[(i + NUM_MODELS // 2) // NUM_MODELS_PER_PROCESS][(i + NUM_MODELS // 2) % NUM_MODELS_PER_PROCESS] = outs[i // NUM_MODELS_PER_PROCESS][i % NUM_MODELS_PER_PROCESS]
+
+            # mutation
+            for param in conv1s[(i + NUM_MODELS // 2) // NUM_MODELS_PER_PROCESS][(i + NUM_MODELS // 2) % NUM_MODELS_PER_PROCESS].parameters():
+                param.data += MUTATION_POWER * torch.randn_like(param)
+            for param in conv2s[(i + NUM_MODELS // 2) // NUM_MODELS_PER_PROCESS][(i + NUM_MODELS // 2) % NUM_MODELS_PER_PROCESS].parameters():
+                param.data += MUTATION_POWER * torch.randn_like(param)
+            for param in outs[(i + NUM_MODELS // 2) // NUM_MODELS_PER_PROCESS][(i + NUM_MODELS // 2) % NUM_MODELS_PER_PROCESS].parameters():
+                param.data += MUTATION_POWER * torch.randn_like(param)
+
+        # 4th quarter
+        for i in range(NUM_MODELS * 3 // 4, NUM_MODELS):
+            conv1s[i // NUM_MODELS_PER_PROCESS][i % NUM_MODELS_PER_PROCESS] = nn.Sequential(
+                nn.Conv2d(1, 1, 16, 1, 2),
+                nn.MaxPool2d(2)
+            ).to(device)
+            conv2s[i // NUM_MODELS_PER_PROCESS][i % NUM_MODELS_PER_PROCESS] = nn.Sequential(
+                nn.Conv2d(1, 1, 4, 1, 2),
+                nn.MaxPool2d(3)
+            ).to(device)
+            outs[i // NUM_MODELS_PER_PROCESS][i % NUM_MODELS_PER_PROCESS] = nn.Sequential(
+                nn.Linear(4, 1)
+            ).to(device)
+
+        print("Epoch {}: Mean = {}, Median = {}, Top 50% = {}, Bottom 50% = {}".format(
+            epoch,
+            sum(fitnesses.values()) // NUM_MODELS,
+            vals[(NUM_MODELS + 1) // 2],
+            sum(vals[:NUM_MODELS // 2]) // (NUM_MODELS // 2),
+            sum(vals[NUM_MODELS // 2:]) // (NUM_MODELS // 2)
+        ))
