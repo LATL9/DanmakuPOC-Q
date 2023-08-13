f8d3ff84d71a327c4dd6803373b9b20579f761d3
Zolax9 <87378963+Zolax9@users.noreply.github.com>
2023-08-14 00:27:53 +0100

    change training params

--- src/common.py
+++ src/common.py
@@ -7,7 +7,7 @@ FPS = 30
 
 NUM_BULLETS = 12
 NUM_PROCESSES = 12
-NUM_MODELS = 720 # must be divisible by 4 (to divide into exact quarters)
+NUM_MODELS = 120 # must be divisible by 4 (to divide into exact quarters)
 NUM_MODELS_PER_PROCESS = round(NUM_MODELS / NUM_PROCESSES)
 TRAIN_TIME = 20 # seconds
 MUTATION_POWER = 0.1 # measure mutations change model
