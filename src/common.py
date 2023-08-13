97cbcc87e3151296d1ba51c3b6837e56eff34cab
Zolax9 <87378963+Zolax9@users.noreply.github.com>
2023-08-14 00:14:24 +0100

    change training params

--- src/common.py
+++ src/common.py
@@ -1,13 +1,13 @@
 import torch.multiprocessing as mp
 
-TEST_MODEL = 0 # -1 = train models, dont test; 0- = test xth model by index
+TEST_MODEL = -1 # -1 = train models, dont test; 0- = test xth model by index
 WIDTH = 800
 HEIGHT = 800
 FPS = 30
 
 NUM_BULLETS = 12
 NUM_PROCESSES = 12
-NUM_MODELS = 384 # must be divisible by 4 (to divide into exact quarters)
+NUM_MODELS = 720 # must be divisible by 4 (to divide into exact quarters)
 NUM_MODELS_PER_PROCESS = round(NUM_MODELS / NUM_PROCESSES)
 TRAIN_TIME = 20 # seconds
 MUTATION_POWER = 0.1 # measure mutations change model
