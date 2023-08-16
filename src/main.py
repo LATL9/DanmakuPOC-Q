f244955e1c87693197049804c09d5ed2cf3d98ac
Zolax9 <87378963+Zolax9@users.noreply.github.com>
2023-08-16 15:47:08 +0100

    Main: reset model by setting random paramters instead
    previous approach involved creating a whole new instance, which was undoubtedly slower than just randomising the model's paramters

--- src/main.py
+++ src/main.py
@@ -99,14 +99,8 @@ if __name__ == '__main__':
 
         # 4th quartile
         for i in range(NUM_MODELS * 3 // 4, NUM_MODELS):
-            models[i // NUM_MODELS_PER_PROCESS][i % NUM_MODELS_PER_PROCESS] = nn.Sequential(
-                nn.Conv2d(2, 4, kernel_size=(9, 9), padding=4, bias=False),
-                nn.MaxPool2d((4, 4), stride=4),
-                nn.Conv2d(4, 8, kernel_size=(5, 5), padding=2, bias=False),
-                nn.MaxPool2d((4, 4), stride=4),
-                nn.Flatten(0, -1),
-                nn.Linear(32, 4, bias=False)
-            ).to(device)
+            for param in models[i // NUM_MODELS_PER_PROCESS][i % NUM_MODELS_PER_PROCESS].parameters():
+                param.data = torch.randn_like(param).to(device)
 
         log.write("{}, {}, {}, {}, {}\n".format(
             time.asctime(),
