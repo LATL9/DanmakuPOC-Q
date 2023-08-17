bcf1cb4def10039eef57ce21a336be7fe412da3a
Zolax9 <87378963+Zolax9@users.noreply.github.com>
2023-08-17 01:24:28 +0100

    Common: change params

--- src/common.py
+++ src/common.py
@@ -8,7 +8,7 @@ FPS = 30
 BULLET_SIZE = 12
 PLAYER_SIZE = 12
 
-NUM_BULLETS = 2
+NUM_BULLETS = 1
 NUM_PROCESSES = 8
 NUM_MODELS = 192 # must be divisible by 4 (to divide into exact quarters)
 NUM_MODELS_PER_PROCESS = round(NUM_MODELS / NUM_PROCESSES)
@@ -17,5 +17,5 @@ MUTATION_POWER = 0.1 # measure mutations change model
 
 BULLET_RANDOM = 0
 BULLET_HONE = 1 # NUM_BULLETS represents number of bullets fired per second when BULLET_HONE is used
-BULLET_HONE_SPEED = HEIGHT / (FPS * 4) # will be on screen for at most 2 secs
+BULLET_HONE_SPEED = HEIGHT / (FPS * 3) # will be on screen for at most 3 secs
 BULLET_TYPE = BULLET_HONE # current type
