b6298122450d09ec11082bb7c1f5a2da51c5b21f
Zolax9 <87378963+Zolax9@users.noreply.github.com>
2023-08-17 01:33:40 +0100

    Common: change params

--- src/common.py
+++ src/common.py
@@ -1,6 +1,6 @@
 import torch.multiprocessing as mp
 
-TEST_MODEL = 1 # -1 = train models, dont test; 0- = test xth model by index
+TEST_MODEL = -1 # -1 = train models, dont test; 0- = test xth model by index
 WIDTH = 800
 HEIGHT = 800
 FPS = 30
@@ -10,7 +10,7 @@ PLAYER_SIZE = 12
 
 NUM_BULLETS = 1
 NUM_PROCESSES = 8
-NUM_MODELS = 192 # must be divisible by 4 (to divide into exact quarters)
+NUM_MODELS = 48 # must be divisible by 4 (to divide into exact quarters)
 NUM_MODELS_PER_PROCESS = round(NUM_MODELS / NUM_PROCESSES)
 TRAIN_TIME = 20 # seconds
 MUTATION_POWER = 0.1 # measure mutations change model
