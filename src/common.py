189d9952e9a551213341d06527b55b0c15efc303
zola <kljr9cat@anonaddy.me>
2023-09-12 10:22:32 +0100

    Common: change params

--- src/common.py
+++ src/common.py
@@ -10,9 +10,10 @@ PLAYER_SIZE = 8
 
 NUM_BULLETS = 2
 NUM_PROCESSES = 12
-NUM_MODELS = 96 # must be divisible by 4 (to divide into exact quarters)
+NUM_MODELS = 48 # must be divisible by 4 (to divide into exact quarters)
 NUM_MODELS_PER_PROCESS = round(NUM_MODELS / NUM_PROCESSES)
-TRAIN_TIME = 20 # seconds
+TRAIN_TIME = 10 # seconds
+NUM_TRIALS = 4
 MUTATION_POWER = 0.1 # measure mutations change model
 
 BULLET_RANDOM = 0
