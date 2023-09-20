0539ac5e46e90ad24b99d6c009cbd8c28898fc74
zola <kljr9cat@anonaddy.me>
2023-09-20 23:34:24 +0100

    Common: remove irrelevant params

--- src/common.py
+++ src/common.py
@@ -10,10 +10,8 @@ PLAYER_SIZE = 8
 
 NUM_BULLETS = 2
 NUM_PROCESSES = mp.cpu_count()
-NUM_MODELS = 48 # must be divisible by 4 (to divide into exact quarters)
 NUM_MODELS_PER_PROCESS = round(NUM_MODELS / NUM_PROCESSES)
 TRAIN_TIME = 32 # seconds
-MUTATION_POWER = 0.1 # measure mutations change model
 
 BULLET_RANDOM = 0
 BULLET_HONE = 1 # NUM_BULLETS represents number of bullets fired per second when BULLET_HONE is used
