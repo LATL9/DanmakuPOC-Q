60a3443b064eccb988b2353250b0f4cd00645e21
LATL9 <113098926+LATL9@users.noreply.github.com>
2023-10-03 07:53:48 +0100

    Common: change params

--- src/common.py
+++ src/common.py
@@ -3,20 +3,20 @@ import torch.multiprocessing as mp
 TRAIN_MODEL = True # true = train, false = test
 WIDTH = 800
 HEIGHT = 800
-FPS = 10
+FPS = 12
 
 BULLET_SIZE = 12
 PLAYER_SIZE = 8
 
-NUM_BULLETS = 8
+NUM_BULLETS = 2
 NUM_PROCESSES = mp.cpu_count()
-TRAIN_TIME = 4 # seconds
+TRAIN_TIME = 32 # seconds
 
 BULLET_RANDOM = 0
 BULLET_HONE = 1 # NUM_BULLETS represents number of bullets fired per second when BULLET_HONE is used
 BULLET_HONE_SPEED = HEIGHT / (FPS * 1) # will be on screen for at most 3 secs
 BULLET_TYPE = BULLET_HONE # current type
 
-LEARNING_RATE = 2e-4
-DISCOUNT_RATE = 0.75
-FRAMES_PER_ACTION = 3 # must be > 1, otherwise code in train() in Model will break
+LEARNING_RATE = 1e-5
+DISCOUNT_RATE = 0.9
+FRAMES_PER_ACTION = 4 # must be > 1, otherwise code in train() in Model will break
