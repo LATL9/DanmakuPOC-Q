4a5dcbf348c882c17f13e0a72136738264949a61
LATL9 <113098926+LATL9@users.noreply.github.com>
2023-10-06 22:10:32 +0100

    Common: increase GAME_FPS, FRAMES_PER_ACTION, NUM_BULLETS and decrease BULLET_HONE_SPEED
    increased GAME_FPS and FRAMES_PER_ACTION should result in smoother moves, as the low GAME_FPS set led to very unsmooth gameplay, and the worry about long training times seems to not be an issue.
    
    increased bullets should benefit the model in dodging bullets in high-intensity situations, which - along with slow bullet speed (although this is more Touhou-centric) - are staples of the danmaku that this model should eventually run on.

--- src/common.py
+++ src/common.py
@@ -8,22 +8,22 @@ TRAIN_MODEL = False
 # False = test model
 WIDTH = 800
 HEIGHT = 800
-TRAIN_FPS = 12 # internal FPS to use when training
+TRAIN_FPS = 24 # internal FPS to use when training
 GUI_FPS = 60 # FPS used with Raylib (should be multiple of FPS)
 GAME_FPS = TRAIN_FPS if BUILD_DL or TRAIN_MODEL else GUI_FPS
 
 BULLET_SIZE = 12
 PLAYER_SIZE = 8
 
-NUM_BULLETS = 2
+NUM_BULLETS = 3
 NUM_PROCESSES = mp.cpu_count()
 TRAIN_TIME = 32 # seconds
 
 BULLET_RANDOM = 0
 BULLET_HONE = 1 # NUM_BULLETS represents number of bullets fired per second when BULLET_HONE is used
-BULLET_HONE_SPEED = HEIGHT / (GAME_FPS * 1) # will be on screen for at most 2 secs
+BULLET_HONE_SPEED = HEIGHT / (GAME_FPS * 3) # will be on screen for at most 3 secs
 BULLET_TYPE = BULLET_HONE # current type
 
 LEARNING_RATE = 1e-5
 DISCOUNT_RATE = 0.9
-FRAMES_PER_ACTION = 3 # must be > 1, otherwise code in train() in Model will break
+FRAMES_PER_ACTION = 4 # must be > 1, otherwise code in train() in Model will break
